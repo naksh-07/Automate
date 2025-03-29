abff65cb91973237f0b8c7c375ec9c7716472862
Naveen Amrawanshi <60285924+naksh-07@users.noreply.github.com>
2025-03-29 12:35:52 +0530

    Update generate_env.sh

--- generate_env.sh
+++ generate_env.sh
@@ -8,7 +8,7 @@ declare -A files_content=(
     ["frpc.env"]=""
     ["pop.env"]=$'# RAM allocation (in GB)\nRAM=4\n\n# Maximum disk usage (in GB)\nMAX_DISK=10\n\n# Cache directory path inside Docker\nCACHE_DIR=/data\n\n# Your Solana Public Key (Replace with your actual key)\nSOLANA_PUBKEY=addrs'
     ["node.env"]=""
-    ["multi.env"]=$'# Bandwidth & Storage\nBANDWIDTH_DOWNLOAD=500\nBANDWIDTH_UPLOAD=250\nSTORAGE=10\n\n# User Credentials (Replace with actual values)\nIDENTIFIER=idfr\nPIN=916524'
+    ["nmulti.env"]=$'# Bandwidth & Storage\nBANDWIDTH_DOWNLOAD=500\nBANDWIDTH_UPLOAD=250\nSTORAGE=10\n\n# User Credentials (Replace with actual values)\nIDENTIFIER=idfr\nPIN=916524'
 )
 
 create_and_secure_files() {
